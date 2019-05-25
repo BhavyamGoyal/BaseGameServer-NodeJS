2dec663651a3e5fe130f41fa859a154c1896c816
Bhavyam Goyal <bhavyam@outscal.com>
2019-05-25 17:06:39 +0530

    with basic server Gamplaylogic class needs to be added

---
+++ API/ServerEvents/ServerEvents.js
@@ -0,0 +1,11 @@
+module.exports = {
+    ON_USER_REGISTER:"onUserRegister",
+    ON_PLAYER_JOINED:"onPlayerJoined",
+    ON_CONNECTED:"onConnected",
+    ON_JOIN_GAME:"onJoinGame",
+    ON_GAME_STARTED:"onGameStarted",
+
+    SEND_INPUT:"sendInput",
+    REGISTER_USER:"registerUser",
+    JOIN_ROOM:"joinRoom"
+}
